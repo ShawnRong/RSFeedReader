540bb48a178dbade746a63eeb79b78faeab010da
ShawnRong <rongshawn@gmail.com>
2016-06-19 15:49:09 +0800

    First

---
+++ RSFeedReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSFeedReader
+//
+//  Created by 戎晟 on 16/6/19.
+//  Copyright © 2016年 Shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
